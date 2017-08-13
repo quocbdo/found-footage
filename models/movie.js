6b9c4ff3c6835db8a7bd57a1cf7efdd9bc855149
Quoc Do <quocbdo@gmail.com>
2017-08-13 16:22:12 -0700

    added movie show

--- models/movie.js
+++ models/movie.js
@@ -3,11 +3,11 @@ var Schema = mongoose.Schema;
 
 var movieSchema = new mongoose.Schema({
     id: Number,
-    original_title: {type: String, required: true},
+    title: {type: String, required: true},
     overview: String,
     cast: [String],
     release_date: String,
-    genre: String,
+    genre: [String],
     vote_average: Number
 });
 
