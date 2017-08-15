c81e1fa34d42da679ad073d3dc31dac58b710a27
Kevin Le <captianle@gmail.com>
2017-08-15 11:28:27 -0700

    link2profile, showlistpage, preventedmovie to be added twice to db

--- routes/lists.js
+++ routes/lists.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var listCtrl = require('../controllers/usersLists-controller')
 
-
+router.get('users/:id/list/:id', listCtrl.show)
 
 module.exports = router;
