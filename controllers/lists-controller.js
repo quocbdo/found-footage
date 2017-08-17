c2530d89422cd828bd7012e8d0773a5022bee402
Quoc Do <quocbdo@gmail.com>
2017-08-17 09:46:59 -0700

    updated lists index and show

--- controllers/lists-controller.js
+++ controllers/lists-controller.js
@@ -4,13 +4,15 @@ var request = require('request');
 const rootURL = 'https://api.themoviedb.org/3/';
 
 function index(req, res) {
-  List.find({}, (err, lists) => {
+  List.find({}).populate('user movies').exec((err, lists) => {
     res.render('lists/index', {lists, user: req.user}); 
   });
 }
 
 function show(req, res) {
-
+  List.findById(req.params.id).populate("user movies").exec((err, list) => {
+    res.render('lists/show', {list, user: req.user});
+  });
 }
 
 module.exports = {
