eb7c08f51486db5d44e887ca54e1715e01f05225
Quoc Do <quocbdo@gmail.com>
2017-08-17 11:53:01 -0700

    added cards to user profile

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,7 +1,7 @@
 var User = require('../models/user');
 
 function show(req, res) {
-  User.findById(req.params.id).populate('lists').exec((err, user) => {
+  User.findById(req.params.id).populate({path:'lists', model:'List', populate: {path:'movies', model:'Movie'}}).exec((err, user) => {
     res.render('userProfile/show', {user});
   });
 }
