4e2ded10e0a403fff9a35f3bf9fce5e620f23610
Vincent Tran <vintran1995@gmail.com>
2017-08-17 19:27:19 -0700

    Added filter list option, and edit list genre

--- controllers/lists-controller.js
+++ controllers/lists-controller.js
@@ -3,7 +3,7 @@ var Movie = require('../models/movie');
 
 function index(req, res) {
   List.find({}).populate('user movies').exec((err, lists) => {
-    res.render('lists/index', {lists, user: req.user}); 
+    res.render('lists/index', {lists, user: req.user, genre: null}); 
   });
 }
 
@@ -35,7 +35,15 @@ function deleteComment(req, res) {
 }
 
 function filter(req, res) {
-
+  if(req.body.genres === "General") {
+    List.find({}).populate('user movies').exec((err, lists) => {
+      res.render('lists/index', {lists, user: req.user, genre: req.body.genres});
+    })
+  } else {
+    List.find({genres: req.body.genres}).populate('user movies').exec((err, lists) => {
+      res.render('lists/index', {lists, user: req.user, genre: req.body.genres});
+    });
+  }
 }
 
 
